8c99eb785bfdbe9e18a865c9dab01067ff7b54b5
EngrSameerHani <127790937+EngrSameerHani@users.noreply.github.com>
2026-03-03 13:36:10 +0500

    Update page.tsx

--- src/app/(home)/about-us/events/[id]/page.tsx
+++ src/app/(home)/about-us/events/[id]/page.tsx
@@ -1,73 +1,94 @@
 import { getEventById } from '@/app/lib/getNews'
-import Link from 'next/link'
-import React from 'react'
 
-const SingleEventPage = async (props: { params: Promise<{ id: number }> }) => {
-  const id = (await props.params).id
-  const oneEvent = await getEventById(id)
+import Link from "next/link";
+import React from "react";
+
+/* ✅ Correct params typing (App Router) */
+type PageProps = {
+  params: {
+    id: string;
+  };
+};
+
+const SingleEventPage = async ({ params }: PageProps) => {
+  const id = Number(params.id);
+
+  const oneEvent = await getEventById(id);
+
+  /* ✅ Safety check */
+  if (!oneEvent) {
+    return <div className="container py-5">Event not found</div>;
+  }
+
   return (
     <>
+      {/* ===== Page Title ===== */}
       <section className="page-title page-title-blank bg-white" id="page-title">
-        {' '}
         <div className="container">
-          {' '}
           <div className="row">
-            {' '}
             <div className="col-12 col-lg-6">
-              {' '}
               <div className="title">
-                {' '}
                 <ol className="breadcrumb breadcrumb-long">
-                  {' '}
                   <li className="breadcrumb-item">
-                    <Link href="/">Home</Link>{' '}
-                  </li>{' '}
+                    <Link href="/">Home</Link>
+                  </li>
+
                   <li className="breadcrumb-item">
-                    <Link href="/about-us/events">Events</Link>{' '}
-                  </li>{' '}
-                  <li className="breadcrumb-item active" aria-current="page">
-                    {oneEvent.slug}{' '}
-                  </li>{' '}
-                </ol>{' '}
-              </div>{' '}
-            </div>{' '}
-          </div>{' '}
+                    <Link href="/about-us/events">Events</Link>
+                  </li>
+
+                  <li className="breadcrumb-item active">
+                    {oneEvent.title.rendered}
+                  </li>
+                </ol>
+              </div>
+            </div>
+          </div>
         </div>
       </section>
+
+      {/* ===== Event Content ===== */}
       <section className="blog blog-single" id="blog">
         <div className="container">
           <div className="row">
-            <div className="col-sm-12 col-md-12 col-lg-8">
+            <div className="col-lg-8">
               <div className="blog-entry">
-                <div className="entry-img">
-                  <a href="#">
+
+                {/* ✅ Featured Image (WordPress structure) */}
+                {oneEvent.featured_media_src_url && (
+                  <div className="entry-img">
                     <img
-                      src={oneEvent.image.url}
-                      width={oneEvent.image.width}
-                      height={oneEvent.image.height}
-                      alt="entry image"
+                      src={oneEvent.featured_media_src_url}
+                      alt={oneEvent.title.rendered}
+                      className="img-fluid"
                     />
-                  </a>
-                </div>
+                  </div>
+                )}
+
                 <div className="entry-content">
                   <div className="entry-title">
-                    <h4>{oneEvent.title}</h4>
-                  </div>
-                  <div className="entry-bio">
-                    <div
+                    <h4
                       dangerouslySetInnerHTML={{
-                        __html: oneEvent.description,
+                        __html: oneEvent.title.rendered,
                       }}
-                    ></div>
+                    />
                   </div>
+
+                  <div
+                    className="entry-bio"
+                    dangerouslySetInnerHTML={{
+                      __html: oneEvent.content.rendered,
+                    }}
+                  />
                 </div>
+
               </div>
             </div>
           </div>
         </div>
       </section>
     </>
-  )
-}
+  );
+};
 
-export default SingleEventPage
+export default SingleEventPage;
