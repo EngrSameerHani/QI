5649aea6c77f85ab8a4433629bc49513e3c42882
EngrSameerHani <127790937+EngrSameerHani@users.noreply.github.com>
2026-03-03 13:05:50 +0500

    Chan

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: false,
   images: {
-    domains: ["driveo.pk", "qih.driveo.pk"],
-
-  },
+  domains: ["qih.driveo.pk", "wp.driveo.pk"],
+},
   experimental: {
     serverActions: {
       bodySizeLimit: '20mb',
